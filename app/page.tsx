3e1c0c0cf8938d543ed7f60d9de298583d48c37a
ktc <ktc@503-06.local>
2024-11-09 15:51:47 +0900

    6回目のコミット

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,7 @@ const data:{
 export default function Home(){
   // JavaScriptの定数や式などを書くことができる
   return (
+    <>
     <section className={styles.top}>
       <div>
        <h1 className="styles.title">テクノロジーの力で世界を変える</h1>
@@ -53,5 +54,40 @@ export default function Home(){
       height={1200}
       />
     </section>
+    <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <ul>
+          {data.contents.map((article) => (
+            <li key={article.id} className={styles.list}>
+              <div className ={styles.link}>
+                <Image
+                 className={styles.image}
+                 src="/no-image.png"
+                 alt="No Image"
+                 width={1200}
+                 height={630}
+                />
+                <dl className={styles.content}>
+                  <dt className={styles.newsItemTitle}> </dt>
+                    <dd className={styles.meta}>
+                      <span className={styles.tag}></span>
+                      <span className={styles.date}>
+                        <Image
+                         src="/clock.svg"
+                         alt=""
+                         width={16}
+                         height={16}
+                         priority
+                        />
+                        {article.publishedAt}
+                      </span>
+                    </dd>
+                </dl>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
