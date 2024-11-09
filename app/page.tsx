e572155b4abb18de1991f02575193c1dcdb3249e
ktc <ktc@503-06.local>
2024-11-09 14:15:06 +0900

    4回目のコミット

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import styles from "./page.module.css";
+
 export default function Home(){
   // JavaScriptの定数や式などを書くことができる
   return (
    <div>
-    <h1 className="title">テクノロジーの力で世界を変える</h1>
-    <p className="description">
+    <h1 className="styles.title">テクノロジーの力で世界を変える</h1>
+    <p className="styles.description">
       私たちは市場をリードしているグローバルカンパニーです。</p>
   </div>
   );
