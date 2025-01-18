993299f365c97ada8b124b6125df7dc22f7216ce
ktc <ktc@503-06.local>
2025-01-18 15:43:50 +0900

    categoryを大文字にする01

--- app/_components/Article/index.tsx
+++ app/_components/Article/index.tsx
@@ -1,29 +1,29 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import type { News } from '@/app/_libs/microcms';
-import Date from '../Date';
-import Category from '../Category';
-import styles from './index.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import type { News } from "@/app/_libs/microcms";
+import Date from "../Date";
+import Category from "../_Category";
+import styles from "./index.module.css";
 
 type Props = {
-    data: News;
-  };
+  data: News;
+};
 
 export default function Article({ data }: Props) {
-    return (
+  return (
     <main>
       <h1 className={styles.title}>{data.title}</h1>
       <p className={styles.description}>{data.description}</p>
       <div className={styles.meta}>
-      <Link
+        <Link
           href={`/news/category/${data.category.id}`}
           className={styles.categoryLink}
         >
-       <Category category={data.category} />
+          <Category category={data.category} />
         </Link>
-         <Date date={data.publishedAt ?? data.createdAt} />
-        </div>
-        {data.thumbnail && (
+        <Date date={data.publishedAt ?? data.createdAt} />
+      </div>
+      {data.thumbnail && (
         <Image
           src={data.thumbnail.url}
           alt=""
